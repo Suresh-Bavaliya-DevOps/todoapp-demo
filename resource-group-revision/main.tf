4be0fe391151cb8a6cf0147bcaa5c20c895e3ddb
Suresh Bavaliya <suresh.bavaliya565@gmail.com>
2025-07-08 23:28:47 +0530

    stg added

---
+++ resource-group-revision/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "9411e33c-c9ad-489f-961e-fcafe36b53f6"
+}
+
+resource "azurerm_resource_group" "demo-rg" {
+  name     = "suresh-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "demo2-rg" {
+  name     = "suresh-rg"
+  location = "West Europe"
+}
+
+
+resource "azurerm_storage_account" "sureshstb" {
+  name                     = "acstorage123"
+  resource_group_name      = "suresh-rg"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "sureshstb" {
+  name                     = "acstorage123"
+  resource_group_name      = "suresh-rg"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "sureshstb2" {
+  name                     = "acstorage1235"
+  resource_group_name      = "suresh-rg"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
